7eb9c3031cca0ff4c0df133e70d16fb8b370b6db
jakuzzibubbles <jakuzzi263@gmail.com>
2024-10-01 03:28:05 +0200

    Api implementation, axiosInstance

--- frontend/taskmanager/src/pages/Login/Login.jsx
+++ frontend/taskmanager/src/pages/Login/Login.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
 import PasswordInput from "../../components/Input/PasswordInput";
 import  Navbar  from "../../components/Navbar/Navbar";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
+import axios from "axios";
+
 
 export const Login = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
+
+    const navigate = useNavigate();
 
     const handleLogin = async (e) =>{
         e.preventDefault();
@@ -26,7 +31,27 @@ export const Login = () => {
         setError("")
 
         // Login API call
+        try {
+          const response = await axiosInstance.post("/login", {
+            email: email,
+            password: password,
+          });
+
+          // Handle successful login response
+          if (response.data && response.data.accessToken) {
+            localStorage.setItem("token", response.data.accessToken);
+            navigate("/dashboard");
+          }
+        } catch (error) {
+          // Handle login error
+          if (error.response && error.response.data && error.response.data.message) {
+            setError(error.response.data.message);
+          } else {
+            setError("An unexpected error occurred. Please try again.");
+          }
+        }
     };
+
 
   return (
     <>
