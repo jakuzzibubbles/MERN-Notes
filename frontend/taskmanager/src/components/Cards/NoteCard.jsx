d84b6ebd5b88c40c7085d90b8ca9ded8facb9380
jakuzzibubbles <jakuzzi263@gmail.com>
2024-10-02 15:55:30 +0200

    ToastMessage Component

--- frontend/taskmanager/src/components/Cards/NoteCard.jsx
+++ frontend/taskmanager/src/components/Cards/NoteCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import moment from "moment";
 import { MdOutlinePushPin } from "react-icons/md";
 import { MdCreate, MdDelete } from "react-icons/md";
 
@@ -16,7 +16,7 @@ const NoteCard = ({
     <div className="border rounded p-4 bg-green-200 hover:shadow-xl transition-all ease-in-out">
       <div className="flex items-center justify-between">
         <h6 className="text-xs font-medium">{title}</h6>
-        <span className="text-xs text-slate-500">{date}</span>
+        <span className="text-xs text-slate-500">{moment(date).format('Do MMM YYYY')}</span>
       </div>
 
       <MdOutlinePushPin
@@ -28,7 +28,7 @@ const NoteCard = ({
 
       <div className="flex items-center justify-between mt-2">
 
-      <div className="text-xs text-slate-500">{tags}</div>
+      <div className="text-xs text-slate-500">{tags.map((item)=> `#${item} `)}</div>
 
       <div className="flex items-center gap-2">
         <MdCreate className="icon-btn hover text-green-600" onClick={onEdit} />
