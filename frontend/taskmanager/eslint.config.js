0f2accf6b1c062e71693c537abf9ee5aa8a3669e
jakuzzibubbles <jakuzzi263@gmail.com>
2024-10-12 01:59:49 +0200

    update proptypes import

--- frontend/taskmanager/eslint.config.js
+++ frontend/taskmanager/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import PropTypes from 'prop-types'
 
 export default [
   { ignores: ['dist'] },
@@ -28,7 +29,6 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      "react/prop-types": "off",
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
